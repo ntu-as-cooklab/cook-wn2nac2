667f37bfbaf8d1b0358d7800d514f00f3fbf5624
alexp205 <politowicz205@gmail.com>
2016-07-12 06:04:52 -0500

    Added ui elements to application.

--- www/js/app.js
+++ www/js/app.js
@@ -25,3 +25,23 @@ angular.module('starter', ['ionic'])
 
   });
 })
+
+.controller('MapCtrl', function($scope, $ionicLoading, $compile) {
+  $scope.centerOnMe = function() {
+    if(!$scope.map) {
+      return;
+    }
+
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
+
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      $scope.loading.hide();
+    }, function(error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
+});
