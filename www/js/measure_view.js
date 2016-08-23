ff73afd5d6b199cd0f782043232a71cf9c1ef6cc
En Shih <seanstone5923@gmail.com>
2016-08-23 21:17:08 +0800

    Working on measure view

--- www/js/measure_view.js
+++ www/js/measure_view.js
@@ -11,17 +11,15 @@ var duration        = 60000;
 
 var temp_equil_status;
 var humd_equil_status;
-var measureButton, timer_status;
 
 function measure_main()
 {
+    // TODO: improve this hack
     var height = $("#measure-view-container").height() - $("#measure-view-bars").height() - 49; // 49 is tab height
     $("#measure-view-disp").css({ 'height' : height.toString() });
 
     isHome = false;
     console.log("measure_main")
-    temp_equil_status       = document.getElementById("temp-equil-status");
-    humd_equil_status       = document.getElementById("humd-equil-status");
     measureButton           = document.getElementById("start-button");
     timer_status            = document.getElementById("timer-status");
 
@@ -33,6 +31,28 @@ function measure_main()
     startCompass();
 
     $('.ppc-progress-fill').css('transform','rotate('+ 0 +'deg)');
+}
+
+function measure_tab_switch(frameName)
+{
+    $(".measure-view-disp-content").replaceWith(document.importNode(document.querySelector(frameName).content, true));
+}
+
+var duration;
+var weather;
+
+function chooseDuration(element)
+{
+    duration = element.dataset.duration;
+    $(".measure-time-button").removeClass  ("button-calm");
+    element.classList.add     ("button-calm");
+}
+
+function chooseWeather(element)
+{
+    weather = element.dataset.weather;
+    $(".weather-button").removeClass  ("button-calm");
+    element.classList.add     ("button-calm");
 }
 
 var tDif = 3;
@@ -91,66 +111,4 @@ function checkEqm()
       initWait = true;
     }
   }
-}
-
-var duration;
-var weather;
-
-function chooseDuration(element)
-{
-    //console.log(element.dataset.duration);
-    duration = element.dataset.duration;
-    $(".measure-time-button").removeClass  ("button-calm");
-    element.classList.add     ("button-calm");
-}
-
-function chooseWeather(element)
-{
-    //console.log(element.dataset.duration);
-    weather = element.dataset.weather;
-    $(".weather-button").removeClass  ("button-calm");
-    element.classList.add     ("button-calm");
-}
-
-function setMeasureButtonStatus(status)
-{
-    measureButton           = document.getElementById("start-button");
-    var measureButtonText   = document.getElementById("start-button-text");
-    timer_status            = document.getElementById("timer-status");
-    switch (status)
-    {
-        case 0: // Not ready
-            timer_status.innerHTML = "Not ready";
-            measureButton.disabled = true;
-            measureButton.classList.remove  ("button-assertive");
-            measureButton.classList.add     ("button-balanced");
-            measureButtonText.innerHTML = "START";
-            break;
-        case 1: // Ready to start
-            timer_status.innerHTML = "Ready to start";
-            measureButton.disabled = false;
-            measureButton.classList.remove  ("button-assertive");
-            measureButton.classList.add     ("button-balanced");
-            measureButtonText.innerHTML = "START";
-            break;
-        case 2: // Measuring
-            timer_status.innerHTML = "Measuring...";
-            measureButton.disabled = false;
-            measureButton.classList.remove  ("button-balanced");
-            measureButton.classList.add     ("button-assertive");
-            measureButtonText.innerHTML = "ABORT";
-            break;
-        case 3: // Finished
-            timer_status.innerHTML = "Finished";
-            measureButton.disabled = false;
-            measureButton.classList.remove  ("button-assertive");
-            measureButton.classList.add     ("button-balanced");
-            measureButtonText.innerHTML = "START";
-            break;
-    }
-}
-
-function measure_tab_switch(frameName)
-{
-    $(".measure-view-disp-content").replaceWith(document.importNode(document.querySelector(frameName).content, true));
 }
---
+++ www/js/measure_view_measure.js
@@ -0,0 +1,80 @@
+"use strict";
+
+var measureButton, timer_status;
+var newLat, newLong, newDesc;
+
+var toggleMeasurement = function()
+{
+    if ('undefined' !== typeof glbsens.currentMeasurement)
+     { glbsens.currentMeasurement.observing ? glbsens.currentMeasurement.stop() : takeNewMeasurement(); }
+    else
+        takeNewMeasurement();
+};
+
+function takeNewMeasurement()
+{
+    glbsens.currentMeasurement = new WindooMeasurement();
+    glbsens.currentMeasurement.duration = duration;
+    glbsens.currentMeasurement.onTick = onMeasurementTick;
+    glbsens.currentMeasurement.onFinish = onMeasurementFinish;
+    glbsens.currentMeasurement.start();
+    setMeasureButtonStatus(2);
+};
+
+function setMeasureButtonStatus(status)
+{
+    measureButton           = document.getElementById("start-button");
+    var measureButtonText   = document.getElementById("start-button-text");
+    timer_status            = document.getElementById("timer-status");
+    var progress_box        = document.getElementById("progress-box");
+    switch (status)
+    {
+        case 0: // Not ready
+            timer_status.innerHTML = "Not ready";
+            measureButton.disabled = true;
+            measureButton.classList.remove  ("button-assertive");
+            measureButton.classList.add     ("button-balanced");
+            measureButtonText.innerHTML = "START";
+            break;
+        case 1: // Ready to start
+            timer_status.innerHTML = "Ready to start";
+            measureButton.disabled = false;
+            measureButton.classList.remove  ("button-assertive");
+            measureButton.classList.add     ("button-balanced");
+            measureButtonText.innerHTML = "START";
+            break;
+        case 2: // Measuring
+            timer_status.innerHTML = "Measuring...";
+            measureButton.disabled = false;
+            measureButton.classList.remove  ("button-balanced");
+            measureButton.classList.add     ("button-assertive");
+            measureButtonText.innerHTML = "ABORT";
+            break;
+        case 3: // Finished
+            timer_status.innerHTML = "Finished";
+            measureButton.disabled = false;
+            measureButton.classList.remove  ("button-assertive");
+            measureButton.classList.add     ("button-balanced");
+            measureButtonText.innerHTML = "START";
+            break;
+    }
+}
+
+function onMeasurementFinish()
+{
+    newMeasurementDone(glbsens.currentMeasurement);
+    setMeasureButtonStatus(3);
+};
+
+function onMeasurementTick()
+{
+    var elapsed = Date.now() - glbsens.currentMeasurement.timeStarted;
+    var progress = elapsed/glbsens.currentMeasurement.duration;
+    var deg = 360 * progress;
+
+    $('.ppc-progress-fill').css('transform','rotate('+ deg +'deg)');
+
+    progress > 0.5 ? $('.progress-pie-chart').addClass('gt-50') : $('.progress-pie-chart').removeClass('gt-50');
+
+    $('.ppc-percents span').html((progress*100).toFixed(0) + '%' + "<br><br>" + (elapsed/1000).toFixed(0) + "/" + glbsens.currentMeasurement.duration/1000 + "s");
+}
