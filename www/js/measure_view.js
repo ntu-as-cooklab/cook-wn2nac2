e68fb18716e9f446d018e3ace540cb78c58d2d12
Liu, An-Chi <b04209032@ntu.edu.tw>
2016-11-06 01:54:10 +0800

    delete code

--- www/js/measure_view.js
+++ www/js/measure_view.js
@@ -104,8 +104,8 @@ function measure_main()
 
     isHome = false;
     console.log("measure_main")
-    measureButton           = document.getElementById("start-button");
-    timer_status            = document.getElementById("timer-status");
+    measureButton = document.getElementById("start-button");
+    timer_status = document.getElementById("timer-status");
 
     glbsens.windooObservation = new WindooObservation();
     glbsens.windooObservation.enable();
@@ -148,62 +148,4 @@ function chooseWeather(element, index){
         );
         $("#weather").html(parseWeather(glbsens.currentMeasurement.weatherType));
     },200);
-}
-
-var tDif = 3;
-var tReady = false;
-var hDif = 20;
-var hReady = false;
-var initWait = true;
-var timeout = false;
-
-function checkEqm()
-{
-  if (!glb.inMeasureView) {
-    console.log("cancelled");
-    return;
-  }
-  console.log("here");
-  if (initWait) {
-    console.log("in first wait sequence");
-    initWait = false;
-    setTimeout(function() {checkEqm()}, 3000);
-  } else {
-    console.log("past first wait sequence");
-    if (!tReady) {
-      console.log("checking temp");
-      var temp = glbsens.currentMeasurement.temp;
-      if (temp.length >= 5) {
-        if (Math.abs(temp[temp.length - 6] - temp[temp.length - 1]) >= tDif) {
-          timeout = true;
-        } else {
-          tReady = true;
-          onTempEquilStatusChanged(1);
-        }
-      } else {
-        timeout = true;
-      }
-    }
-    if (!hReady) {
-      console.log("checking humd");
-      var humd = glbsens.currentMeasurement.humd;
-      if (humd.length >= 5) {
-        if (Math.abs(humd[humd.length - 6] - humd[humd.length - 1]) >= hDif) {
-          timeout = true;
-        } else {
-          hReady = true;
-          onHumdEquilStatusChanged(1);
-        }
-      } else {
-        timeout = true;
-      }
-    }
-    if (timeout && glb.inMeasureView) {
-      console.log("timing out for refresh");
-      setTimeout(function() {checkEqm()}, 3000);
-    } else {
-      console.log("this should only show once per tab switch");
-      initWait = true;
-    }
-  }
 }
